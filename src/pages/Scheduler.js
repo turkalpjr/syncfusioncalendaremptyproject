ad552c0ea6239ce86c8465c08f198456a0bf7fdf
Türkalp Kucur <turkalp.kucur@gmail.com>
2023-07-07 14:38:37 +0300

    veriler getirilebilecek çalışıyor biçimine getirildi
    veriler getirilebilecek çalışıyor biçimine getirildi

--- src/pages/Scheduler.js
+++ src/pages/Scheduler.js
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
 import { ScheduleComponent, ViewsDirective, ViewDirective, Day, Week, WorkWeek, Month, Agenda, Inject, Resize, DragAndDrop } from '@syncfusion/ej2-react-schedule';
 import { DatePickerComponent } from '@syncfusion/ej2-react-calendars';
-
-import { scheduleData } from '../data/data';
+import { useState, useEffect } from 'react';
+import { DataManager, ODataV4Adaptor, Query } from '@syncfusion/ej2-data';
 
 
 
@@ -21,14 +20,40 @@ export const Scheduler = () => {
         arg.navigation.enable = true;
     };
 
-    return (
-        <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
 
+    const dataManger = new DataManager({
+        url: 'https://localhost:44342/api/calendar/eventsforscheduler',
+        adaptor: new ODataV4Adaptor(),
+        headers: [{ 'Authorization': 'Bearer ' + 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIyNSIsInVuaXF1ZV9uYW1lIjoiU8O8bGV5bWFuICBEw7x6Z8O8biIsInJvbGUiOiJTYWxlc01hbmFnZXIiLCJuYmYiOjE2ODg3MTkyMTUsImV4cCI6MTY4OTE1MTIxNSwiaWF0IjoxNjg4NzE5MjE1fQ.C4aMDiNmT1hFn90RLsKSrplUrBaK2VwTczyoZ-rfN7RXsp2BX_-_bcwpVrceNZtF-Jb7jZVTH3HqSnUXf_vt9w' }]
+    });
+
+
+
+    const eventSettings = { dataSource: dataManger };
+
+    const onDataBinding = (e) => {
+        let items = e.result;
+        let scheduleData = [];
+        if (items.length > 0) {
+            for (let i = 0; i < items.length; i++) {
+                scheduleData.push({
+                    Id: items[i].id,
+                    Subject: items[i].subject,
+                    StartTime: items[i].startTime,
+                    EndTime: items[i].endTime,
+                    IsAllDay: !items[i].startTime,
+                    CategoryColor: items[i].categoryColor
+                });
+            }
+        }
+        e.result = scheduleData;
+    }
+
+    return (
+
+        <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
             <ScheduleComponent
-                height="650px"
-                ref={(schedule) => setScheduleObj(schedule)}
-                selectedDate={new Date(2021, 0, 10)}
-                eventSettings={{ dataSource: scheduleData }}
+                width='100%' height='550px' currentView='Month' eventSettings={eventSettings} dataBinding={onDataBinding}
                 dragStart={onDragStart}
             >
                 <ViewsDirective>
@@ -55,6 +80,6 @@ export const Scheduler = () => {
                     </tbody>
                 </table>
             </PropertyPane>
-        </div>
+        </div >
     )
 }
