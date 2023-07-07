d9e11908d38f76ccd7778bc58295ce11033fb653
Türkalp Kucur <turkalp.kucur@gmail.com>
2023-07-07 18:35:54 +0300

    scheduler devam ediliyor
    scheduler devam ediliyor

--- src/pages/Scheduler.js
+++ src/pages/Scheduler.js
@@ -19,21 +19,26 @@ export const Scheduler = () => {
         // eslint-disable-next-line no-param-reassign
         arg.navigation.enable = true;
     };
+    const Statuses = [30, 3];
+
+    let queryString = '&Statusesstr=' + JSON.stringify(Statuses);
 
 
     const dataManger = new DataManager({
-        url: 'https://localhost:44342/api/calendar/eventsforscheduler',
+        url: 'https://localhost:44342/api/calendar/eventsforscheduler?All=true' + queryString,
         adaptor: new ODataV4Adaptor(),
-        headers: [{ 'Authorization': 'Bearer ' + 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIyNSIsInVuaXF1ZV9uYW1lIjoiU8O8bGV5bWFuICBEw7x6Z8O8biIsInJvbGUiOiJTYWxlc01hbmFnZXIiLCJuYmYiOjE2ODg3MTkyMTUsImV4cCI6MTY4OTE1MTIxNSwiaWF0IjoxNjg4NzE5MjE1fQ.C4aMDiNmT1hFn90RLsKSrplUrBaK2VwTczyoZ-rfN7RXsp2BX_-_bcwpVrceNZtF-Jb7jZVTH3HqSnUXf_vt9w' }]
+        headers: [{ 'Authorization': 'Bearer ' + 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIyNSIsInVuaXF1ZV9uYW1lIjoiU8O8bGV5bWFuICBEw7x6Z8O8biIsInJvbGUiOiJTYWxlc01hbmFnZXIiLCJuYmYiOjE2ODg3MzE4OTMsImV4cCI6MTY4OTE2Mzg5MywiaWF0IjoxNjg4NzMxODkzfQ.uNf5_pqHknumT_Tz0aAEBoCU3JBIPjxxeZJkGzK6Apz78jZa3eCJ2bL3UuPs596FOBkO4UELwQY-qcwN83ImrA' }]
     });
 
 
 
     const eventSettings = { dataSource: dataManger };
 
+
     const onDataBinding = (e) => {
         let items = e.result;
         let scheduleData = [];
+        debugger;
         if (items.length > 0) {
             for (let i = 0; i < items.length; i++) {
                 scheduleData.push({
@@ -49,17 +54,26 @@ export const Scheduler = () => {
         e.result = scheduleData;
     }
 
+    const onCreated = () => {
+        debugger;
+    }
+    const onDestroyed = () => {
+        debugger;
+    }
+
+
     return (
 
         <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-            <ScheduleComponent
-                width='100%' height='550px' currentView='Month' eventSettings={eventSettings} dataBinding={onDataBinding}
+            <ScheduleComponent id="WeaseSchedulerId"
+                width='100%' height='550px' currentView='Month' eventSettings={eventSettings} created={onCreated} destroyed={onDestroyed} dataBinding={onDataBinding}
                 dragStart={onDragStart}
             >
                 <ViewsDirective>
                     {['Day', 'Week', 'WorkWeek', 'Month', 'Agenda'].map((item) => <ViewDirective key={item} option={item} />)}
                 </ViewsDirective>
                 <Inject services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]} />
+
             </ScheduleComponent>
             <PropertyPane>
                 <table
@@ -83,3 +97,5 @@ export const Scheduler = () => {
         </div >
     )
 }
+
+
